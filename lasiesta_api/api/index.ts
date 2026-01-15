d0086a5b1d82c34ed5c517a5bae70db79b8fab59
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2026-01-15 15:58:01 -0300

    feat: update .gitignore, refactor API handler, and adjust TypeScript configuration

--- lasiesta_api/api/index.ts
+++ lasiesta_api/api/index.ts
@@ -1,1 +1,13 @@
-export { default } from "../src/handler";
+import awsLambdaFastify from "@fastify/aws-lambda";
+import { buildApp } from "../src/app";
+
+let proxy: ReturnType<typeof awsLambdaFastify>;
+
+export default async function handler(event: any, context: any, callback: any) {
+  if (!proxy) {
+    const app = await buildApp();
+    proxy = awsLambdaFastify(app);
+  }
+
+  return proxy(event, context, callback);
+}
